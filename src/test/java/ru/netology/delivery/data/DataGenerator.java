db6e633ddadf2d36aa5a1b810c31a0d6aecef793
home <home@MSK-NB-POZ2.ad.kdl-test.ru>
2022-10-09 17:25:51 +0300

    Delete comments from DataGenerator.java

--- src/test/java/ru/netology/delivery/data/DataGenerator.java
+++ src/test/java/ru/netology/delivery/data/DataGenerator.java
@@ -25,11 +25,6 @@ public class DataGenerator {
     }
 
     public static String generateCity(String locale) throws IOException {
-        // TODO: добавить логику для объявления переменной city и задания её значения, генерацию можно выполнить
-        // с помощью Faker, либо используя массив валидных городов и класс Random
-        //Faker faker = new Faker(new Locale(locale));
-        //String city = "Москва";
-        //String city = faker.address().city();
         Charset charset = StandardCharsets.UTF_8;
         String townsFile = "src/test/java/ru/netology/delivery/data/towns.txt";
         ArrayList<String> cities = new ArrayList<>(Files.readAllLines(Paths.get(townsFile), charset));
@@ -40,16 +35,12 @@ public class DataGenerator {
     }
 
     public static String generateName(String locale) {
-        // TODO: добавить логику для объявления переменной name и задания её значения, для генерации можно
-        // использовать Faker
         Faker faker = new Faker(new Locale(locale));
         String name = faker.name().lastName() + " " + faker.name().firstName();
         return name.replace('ё', 'е');
     }
 
     public static String generatePhone(String locale) {
-        // TODO: добавить логику для объявления переменной phone и задания её значения, для генерации можно
-        // использовать Faker
         Faker faker = new Faker(new Locale(locale));
         String phone = faker.phoneNumber().phoneNumber();
         return phone;
@@ -61,10 +52,6 @@ public class DataGenerator {
 
         public static UserInfo generateUser(String locale) throws IOException {
             DataGenerator.UserInfo user = new UserInfo(generateCity(locale), generateName(locale), generatePhone(locale));
-            // TODO: добавить логику для создания пользователя user с использованием методов generateCity(locale),
-            // generateName(locale), generatePhone(locale)
-            //UserInfo user = new UserInfo();
-            //Registration.generateUser(locale).name=generateName(locale);
             return user;
         }
     }
